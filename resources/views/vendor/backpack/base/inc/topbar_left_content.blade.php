d12d2e353da130cae593ff04745b381f919dbce3
Charbel Akiki <charbelakiki1111@gmail.com>
2022-12-15 21:05:22 +0200

    Sellout Payments

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -3,3 +3,12 @@
 {{-- <li class="nav-item px-3"><a class="nav-link" href="#">Dashboard</a></li>
 <li class="nav-item px-3"><a class="nav-link" href="#">Users</a></li>
 <li class="nav-item px-3"><a class="nav-link" href="#">Settings</a></li> --}}
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+
+@if(isset($crud->entity_name))
+    @if($crud->entity_name == 'sellout' and !in_array($crud->getCurrentOperation(), ['list', 'show']))
+        <script src="{{ asset('js/sellout.js') }}"></script>
+    @endif
+@endif
